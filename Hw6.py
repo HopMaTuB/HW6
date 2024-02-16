d5b2501abadb5c739bd183f7c7c631c1a46ee4b9
HopMaTuB <104444165+HopMaTuB@users.noreply.github.com>
2024-02-16 19:03:05 +0200

    Add files via upload

---
+++ Hw6.py
@@ -0,0 +1,89 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+		pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value)>=10:
+            super().__init__(value)
+        else:
+            raise Exception("Phone number must be Longer than 10 numbers")
+        
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self,phone):
+        self.phones.append(Phone(phone))
+
+    def del_phone(self,phone):
+        for p in self.phones:
+            if p.value != phone:
+                self.phones.remove(p)
+    
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone   
+
+    def find(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p.value                      
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data[name]
+
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+
+
+book = AddressBook()
+
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+
+book.add_record(john_record)
+
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+for name, record in book.data.items():
+    print(record)
+
+
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+    # Видалення запису Jane
+book.delete("Jane")
+
